37d828012c6d3aceab0b581c508edfc2a5da2035
Aaromalchekavar <binilbijuchengankal@gmail.com>
2022-11-18 12:09:07 +0530

    SingleChat

--- SingleChatGUI/SingleChat.java
+++ SingleChatGUI/SingleChat.java
@@ -15,53 +15,53 @@ import javax.swing.border.EtchedBorder;
 
 public class SingleChat extends javax.swing.JFrame {
 
-  ServerSocket ss;
-  Socket s;
-  JTextField text = new JTextField();
-  JButton send = new JButton();
-  JTextArea messages = new JTextArea();
-  PrintWriter pout;
-  BufferedReader br;
-  ActionListener al;
-  String ipstring;
+  ServerSocket serversocket;
+  Socket socket;
+  JTextField textfield = new JTextField();
+  JButton sendbutton = new JButton();
+  JTextArea textareaormessages = new JTextArea();
+  PrintWriter printwriter;
+  BufferedReader bufferedreader;
+  ActionListener actionlistener;
+  String inputstring;
   boolean ready2send = false;
   SingleChat pt;
-  boolean HorC;
+  boolean HostorClient;
   String cliOrServ;
 
   public SingleChat(boolean hostOrConnect, String ip) {
-    ipstring = ip;
+    inputstring = ip;
     setLayout(null);
     setSize(500, 500);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    add(text);
-    text.setLocation(5, 5);
-    text.setSize(getWidth() - 40 - 60, 30);
-    text.setEnabled(false);
-    add(send);
-    send.setLocation(10 + text.getWidth(), text.getY());
-    send.setText("Send");
-    send.setSize(60, 30);
-    add(messages);
-    messages.setEditable(false);
-    messages.setBorder(new EtchedBorder());
-    messages.setLocation(5, text.getHeight() + text.getY() + 5);
-    messages.setSize(
+    add(textfield);
+    textfield.setLocation(5, 5);
+    textfield.setSize(getWidth() - 40 - 60, 30);
+    textfield.setEnabled(false);
+    add(sendbutton);
+    sendbutton.setLocation(10 + textfield.getWidth(), textfield.getY());
+    sendbutton.setText("Send");
+    sendbutton.setSize(60, 30);
+    add(textareaormessages);
+    textareaormessages.setEditable(false);
+    textareaormessages.setBorder(new EtchedBorder());
+    textareaormessages.setLocation(5, textfield.getHeight() + textfield.getY() + 5);
+    textareaormessages.setSize(
       getWidth() - 30,
-      getHeight() - text.getY() - text.getHeight() - 50
+      getHeight() - textfield.getY() - textfield.getHeight() - 50
     );
 
-    al =
+    actionlistener =
       new ActionListener() {
         public void actionPerformed(ActionEvent e) {
           ready2send = true;
         }
       };
 
-    send.addActionListener(al);
+    sendbutton.addActionListener(actionlistener);
     pt = this;
-    HorC = hostOrConnect;
-    if (HorC) cliOrServ = "\nServer: "; else cliOrServ = "\nClient: ";
+    HostorClient = hostOrConnect;
+    if (HostorClient) cliOrServ = "\nServer: "; else cliOrServ = "\nClient: ";
     Messenger.start();
   }
 
@@ -75,12 +75,12 @@ public class SingleChat extends javax.swing.JFrame {
     if (inp == 0) {
       new SingleChat(true, null).setVisible(true);
     } else {
-      String ipstring = JOptionPane.showInputDialog(
+      String inputstring = JOptionPane.showInputDialog(
         "Please enter the ip address"
       );
       try {
-        InetAddress.getByName(ipstring);
-        new SingleChat(false, ipstring).setVisible(true);
+        InetAddress.getByName(inputstring);
+        new SingleChat(false, inputstring).setVisible(true);
       } catch (Exception e) {
         JOptionPane.showMessageDialog(null, "Invalid or Unreachable IP");
       }
@@ -90,43 +90,43 @@ public class SingleChat extends javax.swing.JFrame {
   Thread Messenger = new Thread() {
     public void run() {
       try {
-        if (HorC) {
-          messages.setText(
+        if (HostorClient) {
+          textareaormessages.setText(
             "Waiting for an incoming connection.\nEnter my ip at client side.\nMy ip: " +
             InetAddress.getLocalHost().getHostAddress()
           );
-          ss = new ServerSocket(4444);
-          s = ss.accept();
-          s.setKeepAlive(true);
+          serversocket = new ServerSocket(4444);
+          socket = serversocket.accept();
+          socket.setKeepAlive(true);
         } else {
-          messages.setText("Connecting to:" + ipstring + ":9999");
-          s = new Socket(InetAddress.getByName(ipstring), 4444);
+          textareaormessages.setText("Connecting to:" + inputstring + ":9999");
+          socket = new Socket(InetAddress.getByName(inputstring), 4444);
         }
-        text.setEnabled(true);
-        pout = new PrintWriter(s.getOutputStream(), true);
-        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        messages.setText(
-          messages.getText() +
+        textfield.setEnabled(true);
+        printwriter = new PrintWriter(socket.getOutputStream(), true);
+        bufferedreader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        textareaormessages.setText(
+          textareaormessages.getText() +
           "\nConnected to:" +
-          s.getInetAddress().getHostAddress() +
+          socket.getInetAddress().getHostAddress() +
           ":" +
-          s.getPort()
+          socket.getPort()
         );
         while (true) {
           if (ready2send == true) {
-            pout.println(text.getText());
-            messages.setText(messages.getText() + "\nMe: " + text.getText());
-            text.setText("");
+            printwriter.println(textfield.getText());
+            textareaormessages.setText(textareaormessages.getText() + "\nMe: " + textfield.getText());
+            textfield.setText("");
             ready2send = false;
           }
-          if (br.ready()) {
-            messages.setText(messages.getText() + cliOrServ + br.readLine());
+          if (bufferedreader.ready()) {
+            textareaormessages.setText(textareaormessages.getText() + cliOrServ + bufferedreader.readLine());
           }
           Thread.sleep(80);
         }
       } catch (Exception ex) {
         JOptionPane.showMessageDialog(pt, ex.getMessage());
-        messages.setText("Cannot connect!");
+        textareaormessages.setText("Cannot connect!");
         try {
           wait(3000);
         } catch (InterruptedException ex1) {}
